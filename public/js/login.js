8b020dae1aea0946bf196b553ac2c05870328fb7
Caliza <rances.rodriguez@gmail.com>
2023-03-21 15:27:47 -0700

    Fixed path in fetch() in login.js.

--- public/js/login.js
+++ public/js/login.js
@@ -31,7 +31,7 @@ const signupFormHandler = async (event) => {
     const password = document.querySelector('#password-signup').value.trim();
 
     if (name && email && password) {
-        const response = await fetch('/api/user', {
+        const response = await fetch('/api/users', {
             method: 'POST',
             body: JSON.stringify({username: name, email, password }),
             headers: { 'Content-Type': 'application/json' },
